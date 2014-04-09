fe9dcd9ce376a135940f66c7fe956ff523ade396
rockwyc992 <rockwyc992@gmail.com>
2014-04-09 00:37:46 +0000

    ....

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import ce1002.a2.s102502044.*;
 import ce1002.a3.s102502044.*;
 import ce1002.a4.s102502044.*;
 import ce1002.a5.s102502044.*;
+import ce1002.a6.s102502044.*;
 import ce1002.e1.s102502044.*;
 import ce1002.e2.s102502044.*;
 import ce1002.e3.s102502044.*;
@@ -12,6 +13,6 @@ import ce1002.q1.s102502044.*;
 public class Main {
 
     public static void main (String[] argv) {
-        new Q1();
+        new A6();
     }
 }
