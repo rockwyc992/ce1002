21a14cb14ba8a4a7ebea9131317a95d0c79bd0a5
rockwyc992 <rockwyc992@gmail.com>
2014-04-21 19:16:45 +0000

    Cube upload

---
+++ src/cube/AI.java
@@ -0,0 +1,15 @@
+package cube;
+
+class AI {
+
+    Cube[][][] cubes;
+
+    AI(Cube[][][] cubes) {
+        this.cubes = cubes;
+    }
+
+    boolean slove() {
+        return true;
+    }
+
+}
--- src/cube/Point.java
+++ src/cube/Point.java
@@ -1,4 +1,4 @@
-package titan;
+package cube;
 
 class Point {
     
@@ -10,6 +10,18 @@ class Point {
     Point(float x, float y, float z) {
         this.x = x;
         this.y = y;
+        this.z = z;
+    }
+
+    void set_x(float x) {
+        this.x = x;
+    }
+
+    void set_y(float y) {
+        this.y = y;
+    }
+
+    void set_z(float z) {
         this.z = z;
     }
 }
